6875393503749f025dbc8bbd8940c5bd57c59a18
tharaka1997 <66878675+tharaka1997@users.noreply.github.com>
2020-07-27 18:43:53 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,161 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password ="";
+$database = "test_db";
+
+$id="";
+$body_price="";
+$body_title="";
+$paint_price="";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// connect to mysql database
+try{
+    $connect = mysqli_connect($host, $user, $password, $database);
+} catch (mysqli_sql_exception $ex) {
+    echo 'Error';
+}
+
+// get values from the form
+function getPosts()
+{
+    $posts=array();
+	$posts[0]=$_POST['id'];
+    $posts[1]=$_POST['body_price'];
+    $posts[2]=$_POST['body_title'];
+    $posts[3]=$_POST['paint_price'];
+    return $posts;
+}
+
+// Search
+
+if(isset($_POST['search']))
+{
+    $data = getPosts();
+    
+    $search_Query = "SELECT * FROM users WHERE id = $data[0]";
+    
+    $search_Result = mysqli_query($connect, $search_Query);
+    
+    if($search_Result)
+    {
+        if(mysqli_num_rows($search_Result))
+        {
+            while($row = mysqli_fetch_array($search_Result))
+            {
+                $id=$row['id'];
+				$body_price=$row['body_price'];
+				$body_titile=$row['body_title'];
+				$pain_tprice=$row['paint_price'];
+				
+            }
+        }else{
+            echo 'No Data For This Id';
+        }
+    }else{
+        echo 'Result Error';
+    }
+}
+
+
+// Insert
+if(isset($_POST['insert']))
+{
+    $data = getPosts();
+    $insert_Query = "INSERT INTO `users`(`body_price`, `body_title`, `paint_price`) VALUES ('$data[1]','$data[2]',$data[3])";
+    try{
+        $insert_Result = mysqli_query($connect, $insert_Query);
+        
+        if($insert_Result)
+        {
+            if(mysqli_affected_rows($connect) > 0)
+            {
+                echo 'Data Inserted';
+            }else{
+                echo 'Data Not Inserted';
+            }
+        }
+    } catch (Exception $ex) {
+        echo 'Error Insert '.$ex->getMessage();
+    }
+}
+
+// Delete
+if(isset($_POST['delete']))
+{
+    $data = getPosts();
+    $delete_Query = "DELETE FROM `users` WHERE `id` = $data[0]";
+    try{
+        $delete_Result = mysqli_query($connect, $delete_Query);
+        
+        if($delete_Result)
+        {
+            if(mysqli_affected_rows($connect) > 0)
+            {
+                echo 'Data Deleted';
+            }else{
+                echo 'Data Not Deleted';
+            }
+        }
+    } catch (Exception $ex) {
+        echo 'Error Delete '.$ex->getMessage();
+    }
+}
+
+// Edit
+if(isset($_POST['update']))
+{
+    $data = getPosts();
+    $update_Query = "UPDATE `users` SET `body_price`='$data[1]',`body_title`='$data[2]',`paint_price`=$data[3] WHERE `id` = $data[0]";
+    try{
+        $update_Result = mysqli_query($connect, $update_Query);
+        
+        if($update_Result)
+        {
+            if(mysqli_affected_rows($connect) > 0)
+            {
+                echo 'Data Updated';
+            }else{
+                echo 'Data Not Updated';
+            }
+        }
+    } catch (Exception $ex) {
+        echo 'Error Update '.$ex->getMessage();
+    }
+}
+
+
+
+?>
+
+
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>PHP INSERT UPDATE DELETE SEARCH</title>
+    </head>
+    <body>
+        <form action="php_insert_update_delete_search.php" method="post">
+            <input type="number" name="id" placeholder="Id" value="<?php echo $id;?>"><br><br>
+            <input type="text" name="body_price" placeholder="body price" value="<?php echo $body_price;?>"><br><br>
+            <input type="text" name="body_title" placeholder="body title" value="<?php echo $body_title;?>"><br><br>
+            <input type="number" name="paint_price" placeholder="paint price" value="<?php echo $paint_price;?>"><br><br>
+            <div>
+                <!-- Input For Add Values To Database-->
+                <input type="submit" name="insert" value="Add">
+                
+                <!-- Input For Edit Values -->
+                <input type="submit" name="update" value="Update">
+                
+                <!-- Input For Clear Values -->
+                <input type="submit" name="delete" value="Delete">
+                
+                <!-- Input For Find Values With The given ID -->
+                <input type="submit" name="search" value="Find">
+            </div>
+        </form>
+    </body>
+</html>
